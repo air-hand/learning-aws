b3deb7a642ecc8401474f8ad2bbdd0cbed7abb6d
air-hand <45233435+air-hand@users.noreply.github.com>
2023-03-21 18:21:29 +0900

    Revert "chore: delete rds temporary." and change security group.
    This reverts commit 948653b6ff619593eb62d12e84e3847c66923bb9.

--- src/services/database/RDS/aurora_mysql.tf
+++ src/services/database/RDS/aurora_mysql.tf
@@ -8,26 +8,26 @@ data "aws_subnet" "private" {
 }
 
 #tfsec:ignore:aws-rds-encrypt-cluster-storage-data
-#resource "aws_rds_cluster" "aurora-mysql" {
-#  cluster_identifier = local.identifier
-#  allocated_storage         = 5
-#  #  db_cluster_instance_class = "db.t3.micro"
-#  #  storage_type              = "gp2"
-#  engine                 = "aurora-mysql"
-#  engine_mode            = "provisioned"
-#  engine_version         = "8.0.mysql_aurora.3.03.0"
-#  master_username        = "user"
-#  master_password        = aws_secretsmanager_secret_version.db_pass.secret_string
-#  database_name          = "auroradb"
-#  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
-#  skip_final_snapshot    = true
-#  apply_immediately      = true
-#  vpc_security_group_ids = [aws_security_group.mysql_security_group.id]
-#  availability_zones     = sort([for subnet in data.aws_subnet.private : subnet.availability_zone])
-#
-#  lifecycle {
-#    ignore_changes = [
-#      availability_zones,
-#    ]
-#  }
-#}
+resource "aws_rds_cluster" "aurora-mysql" {
+  cluster_identifier = local.identifier
+  # allocated_storage  = 5
+  # db_cluster_instance_class = "db.t3.micro"
+  # storage_type              = "gp2"
+  engine                 = "aurora-mysql"
+  engine_mode            = "provisioned"
+  engine_version         = "8.0.mysql_aurora.3.03.0"
+  master_username        = "user"
+  master_password        = aws_secretsmanager_secret_version.db_pass.secret_string
+  database_name          = "auroradb"
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
+  skip_final_snapshot    = true
+  apply_immediately      = true
+  vpc_security_group_ids = [aws_security_group.mysql_security_group.id]
+  availability_zones     = sort([for subnet in data.aws_subnet.private : subnet.availability_zone])
+
+  lifecycle {
+    ignore_changes = [
+      availability_zones,
+    ]
+  }
+}
