948653b6ff619593eb62d12e84e3847c66923bb9
air-hand <45233435+air-hand@users.noreply.github.com>
2023-03-21 18:16:43 +0900

    chore: delete rds temporary.

--- src/services/database/RDS/rds_misc.tf
+++ src/services/database/RDS/rds_misc.tf
@@ -19,8 +19,35 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
   subnet_ids = data.aws_subnets.private.ids
 }
 
+# TODO: delete
+# terraform state mv ...aws_db_subnet_group.rds_subnet_group ...aws_dub_subnet_group.tmp_subnet_group
+resource "aws_db_subnet_group" "tmp_subnet_group" {
+  name       = "dbsample-subnet-group"
+  subnet_ids = data.aws_subnets.private.ids
+}
+
+# TODO: delete
 resource "aws_security_group" "rds_security_group" {
-  name   = "rds-security-group"
+  name   = "dbsample-rds-security-group"
+  vpc_id = data.aws_vpc.tf.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [data.aws_vpc.tf.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [data.aws_vpc.tf.cidr_block]
+  }
+}
+
+resource "aws_security_group" "mysql_security_group" {
+  name   = "mysql-security-group"
   vpc_id = data.aws_vpc.tf.id
 
   ingress {
--- src/services/database/RDS/rds_mysql.tf
+++ src/services/database/RDS/rds_mysql.tf
@@ -3,20 +3,20 @@ locals {
 }
 
 #tfsec:ignore:aws-rds-encrypt-instance-storage-data
-resource "aws_db_instance" "rds_mysql" {
-  identifier             = local.db_name
-  allocated_storage      = 5
-  instance_class         = "db.t3.micro"
-  storage_type           = "gp2"
-  engine                 = "mysql"
-  engine_version         = "5.7"
-  username               = "user"
-  password               = aws_secretsmanager_secret_version.db_pass.secret_string
-  db_name                = local.db_name
-  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
-  skip_final_snapshot    = true
-  apply_immediately      = true
-  vpc_security_group_ids = [aws_security_group.rds_security_group.id]
-  multi_az               = false
-  availability_zone      = var.az_name
-}
+#resource "aws_db_instance" "rds_mysql" {
+#  identifier             = local.db_name
+#  allocated_storage      = 5
+#  instance_class         = "db.t3.micro"
+#  storage_type           = "gp2"
+#  engine                 = "mysql"
+#  engine_version         = "5.7"
+#  username               = "user"
+#  password               = aws_secretsmanager_secret_version.db_pass.secret_string
+#  db_name                = local.db_name
+#  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
+#  skip_final_snapshot    = true
+#  apply_immediately      = true
+#  vpc_security_group_ids = [aws_security_group.mysql_security_group.id]
+#  multi_az               = false
+#  availability_zone      = var.az_name
+#}
