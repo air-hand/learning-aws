372c69213a1998d65504f6f326b19fb1aefabdd0
air-hand <45233435+air-hand@users.noreply.github.com>
2023-06-27 23:17:41 +0900

    chore: rds deletion_protection + snapshot

--- src/services/database/RDS/rds_mysql.tf
+++ src/services/database/RDS/rds_mysql.tf
@@ -5,40 +5,45 @@ locals {
 
 #tfsec:ignore:aws-rds-encrypt-instance-storage-data
 resource "aws_db_instance" "rds_mysql" {
-  identifier             = local.db_name
-  allocated_storage      = 5
-  instance_class         = "db.t3.micro"
-  storage_type           = "gp2"
-  engine                 = "mysql"
-  engine_version         = "5.7"
-  username               = "user"
-  password               = aws_secretsmanager_secret_version.db_pass.secret_string
-  db_name                = local.db_name
-  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
-  skip_final_snapshot    = true
-  apply_immediately      = true
-  vpc_security_group_ids = [aws_security_group.mysql_security_group.id]
-  multi_az               = false
-  availability_zone      = var.az_name
+  identifier                 = local.db_name
+  allocated_storage          = 5
+  instance_class             = "db.t3.micro"
+  storage_type               = "gp2"
+  engine                     = "mysql"
+  engine_version             = "5.7.41"
+  username                   = "user"
+  password                   = aws_secretsmanager_secret_version.db_pass.secret_string
+  db_name                    = local.db_name
+  db_subnet_group_name       = aws_db_subnet_group.rds_subnet_group.name
+  skip_final_snapshot        = true
+  apply_immediately          = true
+  auto_minor_version_upgrade = false
+  vpc_security_group_ids     = [aws_security_group.mysql_security_group.id]
+  multi_az                   = false
+  availability_zone          = var.az_name
+  backup_retention_period    = 1
 }
 
 #tfsec:ignore:aws-rds-encrypt-instance-storage-data
 resource "aws_db_instance" "rds_mysql2" {
-  identifier             = local.db_name2
-  allocated_storage      = 5
-  instance_class         = "db.t3.micro"
-  storage_type           = "gp2"
-  engine                 = "mysql"
-  engine_version         = "5.7"
-  username               = "user"
-  password               = aws_secretsmanager_secret_version.db_pass.secret_string
-  db_name                = local.db_name
-  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
-  skip_final_snapshot    = true
-  apply_immediately      = true
-  vpc_security_group_ids = [aws_security_group.mysql_security_group.id]
-  multi_az               = false
-  availability_zone      = var.az_name
+  identifier        = local.db_name2
+  allocated_storage = 5
+  instance_class    = "db.t3.micro"
+  storage_type      = "gp2"
+  engine            = "mysql"
+  engine_version    = "8.0"
+  username          = "user"
+  password          = aws_secretsmanager_secret_version.db_pass.secret_string
+  #snapshot_identifier        = aws_db_snapshot.rds_mysql_snapshot.id
+  db_name                    = local.db_name
+  db_subnet_group_name       = aws_db_subnet_group.rds_subnet_group.name
+  skip_final_snapshot        = true
+  apply_immediately          = true
+  auto_minor_version_upgrade = false
+  vpc_security_group_ids     = [aws_security_group.mysql_security_group.id]
+  multi_az                   = false
+  availability_zone          = var.az_name
+  deletion_protection        = true
 }
 
 # route53 weighted record sets + roundrobin for CNAME
@@ -58,4 +63,9 @@ resource "aws_route53_record" "rds_mysql" {
 
   set_identifier = "foo-bar-${each.key}"
   records        = [each.value]
+}
+
+resource "aws_db_snapshot" "rds_mysql_snapshot" {
+  db_instance_identifier = aws_db_instance.rds_mysql.id
+  db_snapshot_identifier = "rds-mysql-snapshot"
 }
